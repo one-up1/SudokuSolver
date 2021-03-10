5f3d1c552d0b9038bd58aad6ebc73277ca0d8738
Ferenc Dykstra <oneup1@gmail.com>
2021-03-10 12:29:36 +0100

    Add project files.

---
+++ SudokuSolver/Logics/Solver.cs
@@ -0,0 +1,22 @@
+﻿using SudokuSolver.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace SudokuSolver.Logics
+{
+    public class Solver
+    {
+        public int[][] Solve(int[][] sudoku)
+        {
+            return sudoku;
+        }
+
+        public int[][] Create(int[][] sudoku)
+        {
+            return sudoku;
+        }
+    }
+}
